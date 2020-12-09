79fa149962cb0ccfe8518019b6110f9eb04be49a
Demid Ilkevic <demid@stytch.com>
2020-12-09 11:12:58 +0200

    init

---
+++ exampleUi/src/main/java/com/stytch/sdk/example/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.stytch.sdk.example
+
+import android.content.res.Resources
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.stytch.sdk.*
+import com.stytch.sdk.api.StytchResult
+
+private const val PROJECT_ID = "project-test-d0dbafe6-a019-47ea-8550-d021c1c76ea9"//"Your Id"
+private const val SECRET = "secret-test-6-ma0PNENqjBVX6Dx2aPUIdhLFObauXx07c="//"Your secret"
+private const val SCHEME = "https"
+private const val HOST = "test.stytch.com"
+
+class MainActivity : AppCompatActivity(), StytchUI.StytchUIListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        configureStytch()
+
+        findViewById<View>(R.id.textView).setOnClickListener {
+            launchStytch()
+        }
+    }
+
+    private fun launchStytch() {
+        StytchUI.instance.showUI(this, this, createCustomization())
+    }
+
+
+    private fun createCustomization(): StytchUICustomization {
+
+        val textColor = R.color.textColor
+        val placeholderTextColor = R.color.placeholderTextColor
+        val inputBackgroundColor = R.color.inputBackgroundColor
+        val backgroundColor = R.color.backgroundColor
+
+        return StytchUICustomization().apply {
+
+            titleStyle.colorId = textColor
+            titleStyle.size = 25.dp
+            titleStyle.font = Typeface.create(null as Typeface?, Typeface.BOLD)
+
+            subtitleStyle.colorId = textColor
+            subtitleStyle.size = 18.dp
+            subtitleStyle.font = Typeface.create(null as Typeface?, Typeface.NORMAL)
+
+            inputTextStyle.colorId = textColor
+            inputTextStyle.size = 16.dp
+            inputTextStyle.font = Typeface.create(null as Typeface?, Typeface.NORMAL)
+            inputHintStyle.colorId = placeholderTextColor
+            inputHintStyle.size = 15.dp
+            inputHintStyle.font = Typeface.create(null as Typeface?, Typeface.NORMAL)
+            inputBackgroundColorId = inputBackgroundColor
+            inputBackgroundBorderColorId = textColor
+            inputCornerRadius = 8.dp
+
+            buttonTextStyle.colorId = R.color.white
+            buttonTextStyle.size = 20.dp
+            buttonTextStyle.font = Typeface.create(null as Typeface?, Typeface.BOLD)
+            buttonBackgroundColorId = textColor
+            buttonCornerRadius = 8.dp
+
+            backgroundId = backgroundColor
+            showTitle = true
+            showSubtitle = true
+            showBrandLogo = true
+        }
+    }
+
+    private fun configureStytch() {
+//        Initialize sdk with required parameters
+        Stytch.instance.configure(PROJECT_ID, SECRET, SCHEME, HOST)
+//        Set environment
+        Stytch.instance.environment = StytchEnvironment.TEST
+//        Setting login method
+        Stytch.instance.loginMethod = StytchLoginMethod.LoginOrSignUp
+    }
+
+
+//    StytchUI.StytchUIListener implementation
+
+    override fun onSuccess(result: StytchResult) {
+        //handle success
+        Log.d("TAG", "onSuccess: $result")
+    }
+
+    override fun onFailure() {
+//        handle error
+        Log.d("TAG", "onFailure()")
+    }
+
+    override fun onEvent(event: StytchEvent) {
+        Log.d("TAG", "onEvent($event)")
+    }
+}
+
+//Extension to convert dp to pixels
+val Number.dp: Float
+    get() = this.toFloat() * Resources.getSystem().displayMetrics.density
